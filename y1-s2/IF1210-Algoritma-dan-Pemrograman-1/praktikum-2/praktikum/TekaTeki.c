3c64af8dd31185a95da7e3efe2806d244f9344de
Nicholas Wise <nicholasaragih@gmail.com>
2025-03-26 19:13:21 +0800

    scored 100 on number 5 praktikum 2

--- y1-s2/IF1210-Algoritma-dan-Pemrograman-1/praktikum-2/praktikum/TekaTeki.c
+++ y1-s2/IF1210-Algoritma-dan-Pemrograman-1/praktikum-2/praktikum/TekaTeki.c
@@ -9,24 +9,35 @@
  * @return Mengembalikan hasil kombinasi C(n, k) dalam bentuk long long
  */
 
-long long fact(int j) {
-    long long res = 1;
-    if (j > 1) {
-
-        for (int i = j; i > 1; i--) {
-            res *= i;
-        }
-        return res;    
-    }
-    else {
-        return 1;
-    }
-}
-
 long long kombinasi(int n, int k) {
     if (k < 0 || k > n) return 0;
+    if (k == n) return 1;
+    long long top = 1, bottom = 1, edge, border;
+
+    long long diff = n - k;
+
+    if (diff > k) {
+        edge = diff;
+        border = k;
+    } else {
+        edge = k;
+        border = diff;
+    }
+
+    for (int i = n; i >= 1; i--) {
+        if (i > edge) {
+            top *= i;
+        } else if (i <= border ) {
+            bottom *= i;
+        } else {
+            bottom *= 1;
+        }
+        
+    }
     
-    long long comb = (fact(n))/ (fact(k) * fact(n-k)); 
+
+    long long comb = top / bottom;
+
     return comb;
     
 }
@@ -41,3 +52,4 @@ long long catalan(int n) {
     long long val = kombinasi(2*n, n) / (n + 1);
     return val;
 }
+
