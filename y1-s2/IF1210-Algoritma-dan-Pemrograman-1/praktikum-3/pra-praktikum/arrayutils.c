3e7cabf318897c16303276a4afdd6554dfa7a5af
Nicholas Wise <nicholasaragih@gmail.com>
2025-03-27 02:16:43 +0800

    NOMOR 2 KELAR BOS

---
+++ y1-s2/IF1210-Algoritma-dan-Pemrograman-1/praktikum-3/pra-praktikum/arrayutils.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include "arrayutils.h"
+
+void sort(int arr[] , int n){
+    int temp;
+    //izin bubble sort ketua!
+    for (int i = 0 ; i<n; i++){
+        for (int i = 0 ; i <n -1 ; i++) {
+            if (arr[i] > arr[i+1]){
+                temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+    }
+}
+
+int find(int arr[], int n, int target){
+    int found;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) found = i;
+    }
+    found = -1;
+
+    return found;
+
+}
+
+double calculateAverage(int arr[], int n){
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    return sum / n;
+
+}
+
+int findMedian(int arr[], int n){
+    sort(arr, n);
+
+    int median;
+
+    if (n % 2 == 1) {
+        median = arr[n/2];
+    } else {
+        median = (arr[n/2 - 1] + arr[n/2]) / 2;
+    }
+    
+    return median;
+}
+
+int findMode(int arr[], int n){
+    int el;
+    int modus = 0, max = 0, count;
+
+    for (int i = 0; i < n; i++) {
+        count = 0;
+        for (int j = 0; j < n; j++) {
+            if (arr[j] == arr[j]) count++;
+        }
+        if (count > max) max = count;
+    }
+
+}
+
+int findMax(int arr[], int n){
+    int max = arr[0];
+    for (int i = 0; i < n; i++){
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+int findMin(int arr[], int n){
+    int min = arr[0];
+    for (int i =0 ;i <n ; i++){
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    
+    return min;
+}
+
+void reverseArray(int arr[], int n){
+    int kiri =0 , kanan = n-1;
+
+    while (kiri < kanan){
+        int temp = arr [kiri];
+        arr[kiri] = arr[kanan] ;
+        arr[kanan] = temp;
+
+        kiri++ ; 
+        kanan-- ; 
+    }
+}
