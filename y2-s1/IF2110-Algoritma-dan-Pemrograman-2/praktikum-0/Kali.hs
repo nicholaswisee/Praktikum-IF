acd8c080b35b7e68fa02375876eef8532a9c6123
Nicholas Wise <nicholasaragih@gmail.com>
2025-09-04 23:41:28 +0700

    feat: add prak 0 alpro2

---
+++ y2-s1/IF2110-Algoritma-dan-Pemrograman-2/praktikum-0/Kali.hs
@@ -0,0 +1,14 @@
+-- KALI
+
+-- HEADER
+module Kali where
+
+-- DEFINISI DAN SPESIFIKASI
+kali :: Int -> Int -> Int
+-- kali a b menghasilkan a * b
+-- a dan b adalah integer
+
+-- REALISASI
+kali a b = a * b
+
+-- APLIKASI
---
+++ y2-s1/IF2110-Algoritma-dan-Pemrograman-2/praktikum-0/Pangkat2.hs
@@ -0,0 +1,12 @@
+-- Pangkat2
+
+-- HEADER
+module Pangkat2 where
+
+-- DEFINISI DAN SPESIFIKASI
+pangkat2 :: Int -> Int
+-- pangkat2 a menghasilkan a^2
+-- a adalah integer
+
+-- REALISASI
+pangkat2 a = a * a
